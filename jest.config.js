5f08b598a97c66d24a0cdd318c50926b1bc543e5
leprechaun <leprechaun@gmail.com>
2021-09-23 22:26:55 +0700

    pipeline: Add coverage and a tab for results

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,17 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  reporters: [
+    'default', // keep the default reporter
+      [
+        'jest-xunit',
+        {
+          outputPath: "./coverage",
+          traitsRegex: [
+            { regex: /\(Test Type:([^,)]+)(,|\)).*/g, name: 'Category' },
+            { regex: /.*Test Traits: ([^)]+)\).*/g, name: 'Type' }
+          ]
+        }
+      ]
+  ]
 };
